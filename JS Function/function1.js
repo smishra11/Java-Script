105bb885eacab29fd7fe5cd684faf8959f55918a
smishra11 <msubhasish11@gmail.com>
2019-12-08 17:46:13 +0530

    Added user defined method

--- JS Function/function1.js
+++ JS Function/function1.js
@@ -2,15 +2,28 @@
 //Example x = 54321;
 //Expected Output : 12345
 
-//Build- in method
+//Pre-define method
 document.querySelector('h1').innerHTML = 'Reverse Number';
 
 function reverseNumber(number) {
 
-    number = number + "";   //converted to string
+    number = number.toString();   //converted to string (number = number + "")
 
 	return number.split("").reverse().join("");
 }
 console.log(reverseNumber(54321));
 
 //User Defined
+function reverse_number(num) {
+    let n = num.toString();
+
+    const number = [];
+    let str = "";
+
+    for(let i = n.length-1,k = 0;i>=0;i--,k++) {
+        number[k] = n[i];
+        str = str + number[k];
+    }
+    return str;
+}
+console.log(reverse_number(4321));
