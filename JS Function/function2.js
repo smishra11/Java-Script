3176dccc5acda7ca907978fa52e5cf3a9c97dbef
smishra11 <msubhasish11@gmail.com>
2019-12-09 17:49:17 +0530

    Check the passed string is palindrome or not.

---
+++ JS Function/function2.js
@@ -0,0 +1,40 @@
+//Write a JavaScript function that checks whether a passed string is palindrome or not
+
+//Pre-defined Method
+
+const str = 'MaDamn';
+
+function palindrome(str) {
+    var reg = /\W_/g;
+
+    var smallStr = str.toLowerCase().replace(reg, "");
+
+    let reversed = smallStr.split('').reverse().join('');
+    if(smallStr === reversed) {
+        console.log(reversed);
+        return 'It is a Palindrome.';
+    } else {
+        console.log(reversed);
+        return 'It is not Palindrome and check your input.';
+    }
+}
+
+console.log(palindrome(str));
+
+//User Defined
+
+const string = 'I did, did I ?';
+
+const isPalindrome = function(inStr) {
+    //inStr = inStr.replace(/\W/g, '');
+    inStr = inStr.toLowerCase().replace(/\W/g, '');
+    console.log(inStr);
+    for (let i=0; i<inStr.length; i+=1) {
+        if(inStr[i] !== inStr[inStr.length - 1 - i]) {
+            return false;
+        }
+    }
+     return true;
+}
+
+console.log(isPalindrome(string));
