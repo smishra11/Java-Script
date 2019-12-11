614de336c8f3ae5fd636dca1df490897c0f6b7ca
smishra11 <msubhasish11@gmail.com>
2019-12-11 17:58:57 +0530

    Find the longest word within the string...

--- JS Function/function6.js
+++ JS Function/function6.js
@@ -17,18 +17,36 @@ console.log(longestword(string));
 
 //User Defined Method
 
-const str = 'Web Development Word'
+// const str = 'Web Development Word';
 
-function longest_word(str) {
-    var array1 = str.match(/\W[a-z]{0,}/gi);
-    var result = array1[0];
+// function longest_word(str) {
+//     let array1 = str.match(/\W[a-z]{0,}/gi);
+//      let result = array1[0];
 
-    for(var i = 0; i < array1.length; i++) {
-        if(result.length < array1.length) {
-            result = array1[i];
+//      for(let i = 0; i < array1.length; i++) {
+//          if(result.length < array1.length) {
+//              result = array1[i];
+//                      }
+//      }
+//      return result;
+//  }
+
+//  console.log(longest_word(str));
+
+const str = 'A cat can be good to kill a mouse'
+
+function longest_word(str) { 
+
+    let array1 = str.split(" ");
+    let v = array1[0];
+
+    for(i = 1; i < array1.length; i++) {
+        if(v.length < array1[i].length) {
+            v = array1[i];
         }
-    }
-    return result;
+    } 
+    return v;
 }
 
-console.log(longest_word(str));
+let result = longest_word(str);
+console.log(result);
