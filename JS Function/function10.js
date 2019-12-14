4af5f73b292473a69d240cbf4a051c663e93f34d
smishra11 <msubhasish11@gmail.com>
2019-12-15 00:03:58 +0530

    Print a identity matrix of n row by n column.

---
+++ JS Function/function10.js
@@ -0,0 +1,17 @@
+//Write a JavaScript function which returns the n rows by n columns identity matrix.
+const n = 3;
+
+function matrix(n) {
+
+    for (i=0; i < n; i++) {
+        for (j=0; j < n; j++) {
+            if (i === j) {
+                console.log(' 1 ');
+            } else {
+                console.log(' 0 ');
+            }
+        }
+         console.log('----------');
+    }
+ }
+matrix(n);
