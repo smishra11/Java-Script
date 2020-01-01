d1897e64f032b978ed840ae132631955716ab4bf
smishra11 <msubhasish11@gmail.com>
2020-01-01 23:15:53 +0530

    Searching array with binary search.

--- JS Function/function20.js
+++ JS Function/function20.js
@@ -5,19 +5,19 @@
 let nums = [-1,0,3,5,9,12];
 let target = 9;
 
-var search = function(nums, target) {
+ function search(nums, target) {
   let left = 0;
   let right = nums.length - 1;
 
   while(left <= right) {
-    const mid = left + Math.floor((right - left) /2);
+    const mid = Math.floor((right + left) /2);
 
     if(nums[mid] === target) {
       return mid;
     } else if(nums[mid] < target) {
       left = mid + 1;
     } else {
-      left = mid - 1;
+      left = mid - 1;  
     }
   }
   //Did not found target value
