0aa01c4abd5a8adf285c6090c8f3ffd6c69bda11
smishra11 <msubhasish11@gmail.com>
2020-01-03 23:26:28 +0530

    Return array elements larger than a number.

--- JS Function/function21.js
+++ JS Function/function21.js
@@ -1,4 +1,4 @@
-//Write a JavaScript function that returns array elements larger than a number
+//Write a JavaScript function that returns array elements larger than a number.
 
 let arr = [5,7,14,26,46,9,71];
 let num = 10;
@@ -6,8 +6,8 @@ let num = 10;
 function check(arr) {
   let arr1 = [];
   for(i = 0; i <arr.length; i++) {
-    if(arr[i] >= 10) {
-      arr1 += arr[i];
+    if(arr[i] >= num) {
+      arr1.push(arr[i]);
     }
   }
   return arr1;
