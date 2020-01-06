9c371dd9d8412300bb10e1ba1cd5f58e300504b1
smishra11 <msubhasish11@gmail.com>
2020-01-06 20:35:12 +0530

    Generate a string id of random characters in a specified length.

--- JS Function/function24.js
+++ JS Function/function24.js
@@ -1,11 +1,11 @@
 //Write a JavaScript function that generates a string id (specified length) of random characters
 
 function generateId(num) {
-  let text = "";
-  let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = '';
+  let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for(i = 0; i < num; i++) {
-    text += char.charAt(Math.floor(Math.random() * char.length));
+    text = text + char.charAt(Math.floor(Math.random() * char.length));
   }
   return text;
 }
-console.log(generateId(6));
+console.log(generateId(7));
