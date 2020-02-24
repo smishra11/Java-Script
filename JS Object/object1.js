d4996123ad7d11c81939a30b8f8c022b0259f6aa
smishra11 <msubhasish11@gmail.com>
2020-02-24 23:13:48 +0530

    Write a program to list the properties of a object

---
+++ JS Object/object1.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to list the properties of a JavaScript object
+
+let myObj = {
+  id : 2,
+  name : "Tapu",
+  age : 30,
+  address : 'HYD'
+}
+console.log(Object.getOwnPropertyNames(myObj))
