171dfb3e8dc13be6793048034fc80d640cfc8496
smishra11 <msubhasish11@gmail.com>
2020-02-25 23:36:46 +0530

    Write a JavaScript program to display the reading status of the given books

---
+++ JS Object/object4.js
@@ -0,0 +1,28 @@
+// Write a JavaScript program to display the reading status 
+//(i.e. display book name, author name and reading status) of the following books.
+
+var library = [ 
+  {
+      author: 'Bill Gates',
+      title: 'The Road Ahead',
+      readingStatus: true
+  },
+  {
+      author: 'Steve Jobs',
+      title: 'Walter Isaacson',
+      readingStatus: true
+  },
+  {
+      author: 'Suzanne Collins',
+      title:  'Mockingjay: The Final Book of The Hunger Games', 
+      readingStatus: false
+  }];
+
+  for(i = 0; i < library.length; i ++) {
+    var books = " '" + library[i].title + "' " + "by" + library[i].author + "."
+    if(library[i].readingStatus === true) {
+      console.log("Already read" + books);
+    } else {
+      console.log("You still need to read" + books);
+    }
+  }
