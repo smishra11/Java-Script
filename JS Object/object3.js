0c5d7bd8867e3de8274af0bea41a6c72ebb1ed88
smishra11 <msubhasish11@gmail.com>
2020-08-28 11:07:04 +0530

    Find the length of js object

--- JS Object/object3.js
+++ JS Object/object3.js
@@ -1,16 +1,17 @@
 //Write a JavaScript program to get the length of a JavaScript object.
 
 var student = {
-  name : "David",
-  class : "VI",
-  rollno : 12
-}
+  name: "David",
+  class: "VI",
+  rollno: 12,
+};
 
 //User defined
-Object.size = function(student) {
-  var objsize = 0, key;
+Object.size = (student) => {
+  var objsize = 0,
+    key;
   for (key in student) {
-      if (student.hasOwnProperty(key)) objsize++;
+    if (student.hasOwnProperty(key)) objsize++;
   }
   console.log(objsize);
   return objsize;
