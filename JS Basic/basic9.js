77784e5132b19d76b16336017614642ed331c946
smishra11 <msubhasish11@gmail.com>
2020-11-21 22:41:45 +0530

    Write a JavaScript program to convert temperatures to and from celsius, fahrenheit

---
+++ JS Basic/basic9.js
@@ -0,0 +1,13 @@
+function cToF(celsius) {
+  let cToFahr = (celsius * 9) / 5 + 32;
+  console.log(celsius + 'C is ' + cToFahr + ' F');
+}
+
+cToF(60);
+
+function fToC(fahrenheit) {
+  let fToCel = ((fahrenheit - 32) * 5) / 9;
+  console.log(fahrenheit + 'F is ' + fToCel + ' C');
+}
+
+fToC(45);
